2141b6d94134493e0b32c1652bfbcb18fb462455
gabriel ferreira <gabriel.cruzeiro14@gmail.com>
2024-10-10 14:09:56 -0300

    programa finalizado

--- gerenciamentoBiblioteca/Program.cs
+++ gerenciamentoBiblioteca/Program.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata;
 
 class Biblioteca
 {
-    static List<string> livrosDisponiveis = new List<string> {"Livro A", "Livro B", "Livro C"};
-    
-    static Dictionary<string, int> livrosEmprestados = new Dictionary<string, int>(); 
-
+    static List<string> livrosDisponiveis = new List<string> { "Livro A", "Livro B", "Livro C" };
+    static Dictionary<string, int> livrosEmprestados = new Dictionary<string, int>();
     static Dictionary<string, int> limitePorUsuario = new Dictionary<string, int>();
 
     const int limiteLivrosUsuario = 3;
     const int limiteLivrosAdmin = 10;
 
-    static void Main (string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Bem-vindo à Biblioteca!");
         Console.WriteLine("Você é um administrador ou usuário?");
@@ -29,63 +26,59 @@ class Biblioteca
         }
         else
         {
-            Console.WriteLine("Digite seu nome de usuario: ");
+            Console.WriteLine("Digite seu nome de usuário: ");
             nomeUsuario = Console.ReadLine();
-            if(!limitePorUsuario.ContainsKey(nomeUsuario))
+            if (!limitePorUsuario.ContainsKey(nomeUsuario))
             {
                 limitePorUsuario[nomeUsuario] = 0;
             }
         }
 
-                int limiteLivros = administrador ? limiteLivrosAdmin : limiteLivrosUsuario;
-                
-                string opcao;
-                do
-                {
-                    ExibirMenu();
-                    opcao = Console.ReadLine();
-                    switch (opcao)
-                    {
-                        case "1":
-                        ListarLivros();
-                        break;
-                        case "2":
-                        
-                        EmprestarLivro(nomeUsuario, limiteLivros);
-                            break;
-                            case "3":
+        int limiteLivros = administrador ? limiteLivrosAdmin : limiteLivrosUsuario;
+        string opcao;
 
-                            DevolverLivro(nomeUsuario);
-                            break;
-                            case "4":
-
-                            VerificarLimite(nomeUsuario, limiteLivros);
-                            break;
-                            case "0":
-
-                            Console.WriteLine("Saindo...");
-                            break;
-                            default:
-                            Console.WriteLine("Opção inválida, tente novamente.");
-                            break;
-                    }
-                } while (opcao !="0");  
+        do
+        {
+            ExibirMenu();
+            opcao = Console.ReadLine();
+            switch (opcao)
+            {
+                case "1":
+                    ListarLivros();
+                    break;
+                case "2":
+                    EmprestarLivro(nomeUsuario, limiteLivros);
+                    break;
+                case "3":
+                    DevolverLivro(nomeUsuario);
+                    break;
+                case "4":
+                    VerificarLimite(nomeUsuario, limiteLivros);
+                    break;
+                case "0":
+                    Console.WriteLine("Saindo...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                    break;
+            }
+        } while (opcao != "0");
     }
 
     static void ExibirMenu()
     {
         Console.WriteLine("\nMenu de opções: ");
-        Console.WriteLine("1- Listar livros disponíveis");
-        Console.WriteLine("2- Emprestar livro");
-        Console.WriteLine("3- Devolver livro");
-        Console.WriteLine("4- Verificar limite de empréstimos");
-        Console.WriteLine("0- Sair");
+        Console.WriteLine("1 - Listar livros disponíveis");
+        Console.WriteLine("2 - Emprestar livro");
+        Console.WriteLine("3 - Devolver livro");
+        Console.WriteLine("4 - Verificar limite de empréstimos");
+        Console.WriteLine("0 - Sair");
         Console.WriteLine("Escolha uma opção: ");
     }
-    
+
     static void ListarLivros()
     {
-        Console.WriteLine("n\LivrosDisponiveis: ");
+        Console.WriteLine("\nLivros Disponíveis: ");
         foreach (var livro in livrosDisponiveis)
         {
             Console.WriteLine(livro);
@@ -94,62 +87,52 @@ class Biblioteca
 
     static void EmprestarLivro(string usuario, int limite)
     {
-        Console.WriteLine($"Você atingiu o limite de {limite} livros emprestados.");
-        return;
-    }
-
-    Console.WriteLine("Digite o nome do livro que deseja emprestar: ");
-    string livro = Console.ReadLine();
-
-    if (livrosDisponiveis.Contains(livro))
-    {
-        livrosDisponiveis.Remove(livro);
-        if (!livrosEmprestados.ContainsKey(livro))
+        if (limitePorUsuario[usuario] >= limite)
         {
-            livrosEmprestados[livro] = 0;
+            Console.WriteLine($"Você atingiu o limite de {limite} livros emprestados.");
+            return;
         }
-        livrosEmprestados[livro]++;
-        limitePorUsuario[usuario]++;
-        Console.WriteLine($"Você emprestou '{livro}'.");
+
+        Console.WriteLine("Digite o nome do livro que deseja emprestar: ");
+        string livro = Console.ReadLine();
+
+        if (livrosDisponiveis.Contains(livro))
+        {
+            livrosDisponiveis.Remove(livro);
+            if (!livrosEmprestados.ContainsKey(livro))
+            {
+                livrosEmprestados[livro] = 0;
+            }
+            livrosEmprestados[livro]++;
+            limitePorUsuario[usuario]++;
+            Console.WriteLine($"Você emprestou '{livro}'.");
+        }
+        else
+        {
+            Console.WriteLine("Livro não disponível.");
+        }
     }
-    else
+
+    static void DevolverLivro(string usuario)
     {
-        Console.WriteLine("Livro não disponível.");
+        Console.WriteLine("Digite o nome do livro que deseja devolver: ");
+        string livro = Console.ReadLine();
+
+        if (livrosEmprestados.ContainsKey(livro) && livrosEmprestados[livro] > 0)
+        {
+            livrosEmprestados[livro]--;
+            limitePorUsuario[usuario]--;
+            livrosDisponiveis.Add(livro);
+            Console.WriteLine($"Você devolveu '{livro}'.");
+        }
+        else
+        {
+            Console.WriteLine("Livro não encontrado nos empréstimos.");
+        }
+    }
+
+    static void VerificarLimite(string usuario, int limite)
+    {
+        Console.WriteLine($"Você emprestou {limitePorUsuario[usuario]} de {limite} livros permitidos.");
     }
 }
-
-static void DevolverLivro(string usuario)
-{
-    Console.WriteLine("Digite o nome do livro que deseja devolver: ");
-    string livro = Console.ReadLine();
-
-
-    if (livrosEmprestados.ContainsKey(livro) && livrosEmprestados[livro] > 0)
-    {
-        livrosEmprestados[livro]--;
-        limitePorUsuario[usuario]--;
-        livrosDisponiveis.Add(livro);
-        Console.WriteLine($"Você devolveu '{livro}'.");
-    }
-    
-
-
-                
-                
-
-                    
-
-                    
-                        
-                        
-                    
-                
-
-
-
-
-
-        
-
-
-
